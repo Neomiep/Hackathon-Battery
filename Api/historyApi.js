81eb68a8c7096323a27dff624e8649a92991f821
neomie peretz <neoperetzmie@hotmail.com>
2018-10-11 13:13:03 +0300

    if-server

--- Api/historyApi.js
+++ Api/historyApi.js
@@ -4,7 +4,7 @@ let User = require('../Models/userModel')
 let History = require("../Models/historyModel")
 
 router.get("/users/history/:userId", (req, res) => {
-    User.find({_id:req.params.userId}, {history:true}).populate("history history.userSelling history.userBuying").exec((err, history) => {
+    User.find({ _id: req.params.userId }, { history: true }).populate("history history.userSelling history.userBuying").exec((err, history) => {
         if (err) {
             res.status(500).send(err);
         }
@@ -16,28 +16,43 @@ router.post('/users/:userBuyingId/:userSellingUsername/history/:amount', (req, r
     let userBuyingId = req.params.userBuyingId
     let userSellingUsername = req.params.userSellingUsername
     let amount = req.params.amount
-    User.find({ userName: userSellingUsername }, { _id: true }, (err, userSellingId)=>{
+    User.find({ userName: userSellingUsername }, { _id: true }, (err, userSellingId) => {
         if (err) { res.status(500).send(err) }
         else {
-            let newHistory = new History({ amount: amount, userBuying: userBuyingId, userSelling: userSellingId, bought:"", sold:"" })
-            newHistory.save((err, history) => {
-                if (err) { res.status(500).send(err) }
-                else {
-                    User.findByIdAndUpdate(userBuyingId, { $push: { "history": history._id } }, { new: true }).exec((err) => {
-                        if (err) { res.status(500).send(err) }
-                        else {
-                            User.findByIdAndUpdate(userSellingId, { $push: { "history": history._id } }, { new: true }).exec((err) => {
-                                if (err) { res.status(500).send(err) }
-                                else {
-                                    res.send(history)
-                                }
-                            })
-                        }
-                    })
+            let newHistory = new History({ amount: amount, userBuying: userBuyingId, userSelling: userSellingId, bought: "", sold: "" })
+            if (userBuyingId == userSellingId[0]._id) {
+                newHistory.save((err, history) => {
+                    if (err) { res.status(500).send(err) }
+                    else {
+                        User.findByIdAndUpdate(userBuyingId, { $push: { "history": history._id } }, { new: true }).exec((err) => {
+                            if (err) { res.status(500).send(err) }
+                            else{
+                                res.send(history)
+                            }
+                        })
+                    }
+                })
+            }
+            else {
+                newHistory.save((err, history) => {
+                    if (err) { res.status(500).send(err) }
+                    else {
+                        User.findByIdAndUpdate(userBuyingId, { $push: { "history": history._id } }, { new: true }).exec((err) => {
+                            if (err) { res.status(500).send(err) }
+                            else {
+                                User.findByIdAndUpdate(userSellingId, { $push: { "history": history._id } }, { new: true }).exec((err) => {
+                                    if (err) { res.status(500).send(err) }
+                                    else {
+                                        res.send(history)
+                                    }
+                                })
+                            }
+                        })
 
-                }
+                    }
 
-            })
+                })
+            }
         }
 
     })
